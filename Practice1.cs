571ed316f40c187fa06742a679a3e5726e84e68b
Fardin Hasan <fardinhasansheam999@gmail.com>
2026-03-16 17:54:44 +0600

    Add files via upload

---
+++ Practice1.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Practice1
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!, This is my first C# program.");
+    }
+}
---
+++ Practice2.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Practice2
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!, This is my Second C# program.");
+    }
+}
