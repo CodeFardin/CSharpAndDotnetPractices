25403e014ce2fd922cb44a50d12a776e789f9069
Fardin Hasan <fardinhasansheam999@gmail.com>
2026-04-06 22:39:17 +0600

    Created a small project

--- Practice1.cs
+++ Practice1.cs
@@ -1,9 +1,40 @@
-using System;
+ShowTitle();
 
-public class Practice1
+int markOfPhysics = MarksInput("Physics");
+int markOfChemistry = MarksInput("Chemistry");
+int markOfBiology = MarksInput("Biology");
+TotalMark(markOfPhysics, markOfBiology, markOfChemistry);
+AvgMark(markOfPhysics, markOfBiology, markOfChemistry);
+
+void ShowTitle()
 {
-    public static void Main(string[] args)
+    System.Console.WriteLine("________Welcome________");
+    System.Console.WriteLine("Student Result Calculator");
+    System.Console.WriteLine("Enter Your Marks");
+}
+
+int MarksInput(string sub)
+{
+    System.Console.Write($"Enter your number for {sub}: ");
+    bool isValid = int.TryParse(Console.ReadLine(), out int mark);
+    if(!isValid || mark < 0 || mark > 100)
     {
-        Console.WriteLine("Hello World!, This is my first C# program.");
-    }
+        System.Console.WriteLine("Wrong input. Try Again!");
+        MarksInput(sub);
+    } 
+
+    return mark;
+} 
+
+void TotalMark(int mark1, int mark2, int mark3)
+{
+    int totalMarks = mark1 + mark2 + mark3;
+    System.Console.WriteLine($"Total Mark: {totalMarks}");
+
+}
+
+void AvgMark(int mark1, int mark2, int mark3)
+{
+    int avgMarks = (mark1 + mark2 + mark3)/3;
+    System.Console.WriteLine($"Average Mark: {avgMarks}");
 }
